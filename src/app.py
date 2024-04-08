6be9087b7d6d974f02f50e97b59186690bf1025b
xiaoming <lininruc@gmail.com>
2024-04-08 20:28:24 +0800

    Add mvs

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from web3 import Web3
 
 # 连接到以太坊节点，这里使用的是Infura的主网节点
-infura_url = 'https://mainnet.infura.io/v3/xxx'
+infura_url = 'https://mainnet.infura.io/v3/'
 web3 = Web3(Web3.HTTPProvider(infura_url))
 
 # 确保连接成功
@@ -11,7 +11,7 @@ else:
     print("Failed to connect to Ethereum network")
 
 # 替换下面的地址为你想要查询的以太坊地址
-address = "xxx"
+address = "0xDc698F8f9D8D8b5350BE1A5CdE21CA7eCe400E2D"
 
 # 查询余额
 balance = web3.eth.get_balance(address)
@@ -21,20 +21,22 @@ balance_in_eth = web3.from_wei(balance, 'ether')
 
 print(f"The balance of address {address} is: {balance_in_eth} ETH")
 
-import requests
-import json
+######
 
-url = 'https://mainnet.infura.io/v3/285fc71094704eb9a4df74e9abd1eb3e'
+# 查询最新区块的编号
+latest_block_number = web3.eth.block_number
+print(f"Latest block number: {latest_block_number}")
 
-payload = {
-    "jsonrpc": "2.0",
-    "method": "eth_blockNumber",
-    "params": [],
-    "id": 1
-}
-
-headers = {'content-type': 'application/json'}
-
-response = requests.post(url, data=json.dumps(payload), headers=headers).json()
-
-print(response)
+# 查询最新区块的详细信息
+latest_block = web3.eth.get_block(latest_block_number)
+for tx_hash in latest_block['transactions']:
+    print(tx_hash.hex())
+    # 查询特定交易的详细信息
+    transaction_details = web3.eth.get_transaction(tx_hash.hex())
+    # print(f"Transaction details: {transaction_details}")
+    print("======")
+    print("from:" + transaction_details['from'])
+    print("to:" + transaction_details['to'])
+    print("gas:" + str(transaction_details['gas']))
+    print("gasPrice:" + str(transaction_details['gasPrice']))
+    # break
